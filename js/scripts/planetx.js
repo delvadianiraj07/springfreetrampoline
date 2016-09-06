08e2569e386ab568a906f4c0f30d0301de7aa472
Niraj <delvadia_niraj@yahoo.com>
2016-09-06 17:31:08 +0530

    Home Page deskotop changes

--- js/scripts/planetx.js
+++ js/scripts/planetx.js
@@ -3,24 +3,65 @@
 (function($){
 
 
-  $(document).ready(function(jQuery){
-
+  $(document).ready(function(){
+  
     // ============= header top banner =================
     // set cookie for banner for first visit
     var $promoBanner = $('#preheader-promo-banner');
     var activePromoBanner = $.cookie("activePromoBanner");
-
+    
     if (activePromoBanner == null) {
-        $promoBanner.trigger('click');
+      $promoBanner.trigger('click');
     }
     $.cookie('activePromoBanner', 'yes', { expires: 1, path: '/' });
     
     // Promo Banner Toggle
     $promoBanner.click(function(event) {
       $('#promo-banner').toggle( "slow", function() {
-        $promoBanner.find('.glyphicon').toggleClass("glyphicon-menu-down glyphicon-menu-up");
+      $promoBanner.find('.glyphicon').toggleClass("glyphicon-menu-down glyphicon-menu-up");
       });
     });
 
+    // ================= Home
+    $(".ilbvid-tgoma").iLightBox({
+        skin: "springfree",
+        controls: {
+            fullscreen: !1
+        }
+    });
 
-})(jQuery);    
+    $("#vid-testimonial").royalSlider({
+        autoHeight: !0,
+        arrowsNav: !1,
+        controlNavigation: 'bullets',
+        arrowsNavAutoHide: !1,
+        navigateByClick: !1,
+        autoPlay: {
+            enabled: !0,
+            delay: 5e3
+        }
+    });
+
+    // ========= Video Testimonials
+    $(".ilbvid-vidTesti-1").iLightBox({
+        skin: "springfree",
+        controls: {
+            fullscreen: !1
+        }
+    });
+    $(".ilbvid-vidTesti-2").iLightBox({
+        skin: "springfree",
+        controls: {
+            fullscreen: !1
+        }
+    });
+    $(".ilbvid-vidTesti-3").iLightBox({
+        skin: "springfree",
+        controls: {
+            fullscreen: !1
+        }
+    });
+  
+  });
+
+})(jQuery);
