55070a1e2f833236520a50bd04f363d80f89752d
Niraj <delvadia_niraj@yahoo.com>
2016-09-17 16:54:23 +0530

    Bug Fix

--- index/js/scripts/planetx.js
+++ index/js/scripts/planetx.js
@@ -1,129 +1,119 @@
 //planetx's JS
 
-(function($){
+(function($) {
 
 
-  $(document).ready(function(){
+    $(document).ready(function() {
 
-    var s = $("#header .header-holder");
-    var pos = s.position();                    
-    $(window).scroll(function() {
-        var windowpos = $(window).scrollTop();
-        if (windowpos >= pos.top) {
-            s.addClass("stick");
-        } else {
-            s.removeClass("stick"); 
+        stickHeader();
+
+        function stickHeader(){
+            var s = $("#header .header-holder");
+            var pos = s.position();
+
+            $(window).scroll(function() {
+                var windowpos = $(window).scrollTop();
+                if (windowpos >= pos.top) {
+                    s.addClass("stick");
+                } else {
+                    s.removeClass("stick");
+                }
+
+                var main_content = $('#static-panels');
+                var topDistance = main_content.offset().top;
+
+                if ((topDistance - 100) < windowpos) {
+                    $('#header #nav').addClass('trans-header');
+                } else {
+                    $('#header #nav').removeClass('trans-header');
+                }
+            });
         }
+        
+        
 
-        var main_content = $('#static-panels');
-        var topDistance = main_content.offset().top;
+        // ============= header top banner =================
+        // set cookie for banner for first visit
+        var $promoBanner = $('#preheader-promo-banner');
+        var activePromoBanner = $.cookie("activePromoBanner");
 
-        if ( (topDistance - 100) < windowpos ) {
-          $('#header #nav').addClass('trans-header');
+        if (activePromoBanner == null) {
+            $promoBanner.trigger('click');
         }
-        else{
-          $('#header #nav').removeClass('trans-header');
-        }
-     
-    });
-  
-    // ============= header top banner =================
-    // set cookie for banner for first visit
-    var $promoBanner = $('#preheader-promo-banner');
-    var activePromoBanner = $.cookie("activePromoBanner");
-    
-    if (activePromoBanner == null) {
-      $promoBanner.trigger('click');
-    }
-    $.cookie('activePromoBanner', 'yes', { expires: 1, path: '/' });
-    
-    // Promo Banner Toggle
-    $promoBanner.click(function(event) {
-      var open_bnr = $(this).data('open');
-      if (open_bnr) {
-         $('#promo-banner').slideUp('slow', function() {           
-           $promoBanner.find('.glyphicon').addClass('glyphicon-menu-down').removeClass('glyphicon-menu-up');
-         });
-      } else {
-         $('#promo-banner').slideDown('slow', function() {
-           $promoBanner.find('.glyphicon').addClass('glyphicon-menu-up').removeClass('glyphicon-menu-down');
-         });
-      }
-      $(this).data("open", !open_bnr);
+        $.cookie('activePromoBanner', 'yes', { expires: 1, path: '/' });
 
-      /*$('#promo-banner').toggle( "slow", function() {
-        $promoBanner.find('.glyphicon').toggleClass("glyphicon-menu-down glyphicon-menu-up");
-      });*/
+        // Promo Banner Toggle
+        $promoBanner.click(function(event) {
 
-    });
-    $('#close-preheader').click(function(event) {
-      $promoBanner.trigger('click')
-    });
+            var open_bnr = $(this).data('open');
+            if (open_bnr) {
+                $('#promo-banner').slideUp('slow', function() {
+                    $promoBanner.find('.glyphicon').addClass('glyphicon-menu-down').removeClass('glyphicon-menu-up');
+                    stickHeader();
+                });
+            } else {
+                $('#promo-banner').slideDown('slow', function() {
+                    $promoBanner.find('.glyphicon').addClass('glyphicon-menu-up').removeClass('glyphicon-menu-down');
+                    stickHeader();
+                });
+            }
+            $(this).data("open", !open_bnr);
 
-    // ================= Home
-    $(".ilbvid-tgoma").iLightBox({
-        skin: "springfree",
-        controls: {
-            fullscreen: !1
-        }
-    });
-
-    $("#vid-testimonial").royalSlider({
-        autoHeight: !0,
-        arrowsNav: !1,
-        controlNavigation: 'bullets',
-        arrowsNavAutoHide: !1,
-        navigateByClick: !1,
-        autoPlay: {
-            enabled: !0,
-            delay: 5e3
-        }
-    });
-
-    // ========= Trampoline Hotspot box
-    $('#trmp-hotspots .hotspot-point').each(function(index, el) {
-        var x_pos = $(this).data('x');
-        var y_pos = $(this).data('y');
-        $(this).css({
-            top: x_pos + '%',
-            left: y_pos + '%'
+        });
+        $('#close-preheader').click(function(event) {
+            $promoBanner.trigger('click')
         });
 
-        $(this).click(function(event) {
-            event.preventDefault();
-            $(this).siblings().removeClass('open-preview');
-            $(this).addClass('open-preview');
-
-            /*$(this).css('overflow', 'visible');
-            $(this).siblings().css('overflow', 'hidden');
-            $(this).find('.hotspot-tooltip').css('opacity', '1').css('z-index', '999');
-            $(this).siblings().find('.hotspot-tooltip').css('opacity', '0').css('z-index', '9');
-            $(this).find('.hotspot-tooltip').show(500);
-            $(this).siblings().hide(500);
-            $(this).find('.hotspot-tooltip').css('z-index', '999');
-            $(this).siblings().find('.hotspot-tooltip').css('z-index', '9');*/
-
-            //var hs_item = $(this).find('.hs-tooltip-w').html();
-            //$('#hotspots-item').html(hs_item);
-
-            $(this).find('.hotspot-tooltip').click( function(ev){
-                ev.stopPropagation();
-            }); 
-            event.stopPropagation();  
+        // ================= Home
+        $(".ilbvid-tgoma").iLightBox({
+            skin: "springfree",
+            controls: {
+                fullscreen: !1
+            }
         });
-        var hs_item = $(this).find('.hotspot-tooltip').html();
-        $('#hotspots-item').append('<div class="hotspot-tooltip clearfix">' + hs_item + '</div>');
-        //$('#hotspots-item').append(hs_item);
-    });
-    $(document).click( function(e){
-        var $all_hotspot = $('#trmp-hotspots .hotspot-point');
-        $all_hotspot.removeClass('open-preview');
-        /*$all_hotspot.css('overflow', 'hidden');
-        $all_hotspot.find('.hotspot-tooltip').css('opacity', '0').css('z-index', '9');*/
-    });
+
+        $("#vid-testimonial").royalSlider({
+            autoHeight: !0,
+            arrowsNav: !1,
+            controlNavigation: 'bullets',
+            arrowsNavAutoHide: !1,
+            navigateByClick: !1,
+            autoPlay: {
+                enabled: !0,
+                delay: 5e3
+            }
+        });
+
+        // ========= Trampoline Hotspot box
+        $('#trmp-hotspots .hotspot-point').each(function(index, el) {
+            var x_pos = $(this).data('x');
+            var y_pos = $(this).data('y');
+            $(this).css({
+                top: x_pos + '%',
+                left: y_pos + '%'
+            });
+
+            $(this).click(function(event) {
+                event.preventDefault();
+                $(this).siblings().removeClass('open-preview');
+                $(this).addClass('open-preview');
+
+                $(this).find('.hotspot-tooltip').click(function(ev) {
+                    ev.stopPropagation();
+                });
+                event.stopPropagation();
+            });
+            var hs_item = $(this).find('.hotspot-tooltip').html();
+            $('#hotspots-item').append('<div class="hotspot-tooltip clearfix">' + hs_item + '</div>');
+            //$('#hotspots-item').append(hs_item);
+        });
+        $(document).click(function(e) {
+            var $all_hotspot = $('#trmp-hotspots .hotspot-point');
+            $all_hotspot.removeClass('open-preview');
+        });
 
 
-    // =========== Counter 
+        // =========== Counter 
     var initialDate = new Date("9/10/2015");
     var jump = 2;
 
@@ -170,73 +160,103 @@
         diffSeconds = parseInt(diffSeconds) + parseInt(jump);
         var diffSeconds_a = diffSeconds.toString().split('');
         var jump_count = new Array();
+        var jump_count_old = new Array();
+
+        $('#jumpCount span').each(function(index, el) {
+            jump_count_old.push($(this).text());
+        });
         for (var i = 0; i <= diffSeconds_a.length - 1; i++) {
-            jump_count.push('<span>'+ diffSeconds_a[i] +'</span>');              
+            if(diffSeconds_a[i] == jump_count_old[i]){
+                jump_count.push('<span>'+ diffSeconds_a[i] +'</span>');
+            }else{
+                jump_count.push('<span class="flip">'+jump_count_old[i]+'</span>');     
+            }
         }
         $('#jumpCount').html(jump_count);
+        setTimeout(function(){
+            $('#jumpCount span').each(function(index, el) {
+                $(this).text(diffSeconds_a[index]);
+            });
+        },300);
+        
 
         diffSeconds_2 = parseInt(diffSeconds_2) + parseInt(jump);
         var diffSeconds_a_2 = diffSeconds_2.toString().split('');
         var jump_count_2 = new Array();
+        var jump_count_old_2 = new Array();
+
+        $('#caloriCount span').each(function(index, el) {
+            jump_count_old_2.push($(this).text());
+        });
         for (var i = 0; i <= diffSeconds_a_2.length - 1; i++) {
-            jump_count_2.push('<span>'+ diffSeconds_a_2[i] +'</span>');              
+            if(diffSeconds_a_2[i] == jump_count_old_2[i]){
+                jump_count_2.push('<span>'+ diffSeconds_a_2[i] +'</span>');
+            }else{
+                jump_count_2.push('<span class="flip">'+ jump_count_old_2[i] +'</span>');     
+            }         
         }
         $('#caloriCount').html(jump_count_2);
+        setTimeout(function(){
+            $('#caloriCount span').each(function(index, el) {
+                $(this).text(diffSeconds_a_2[index]);
+            });
+        },300);
     }, 1000); 
     
     // ====================================
 
-    $(".ilbvid-trampo-1").iLightBox({
-        skin: "springfree",
-        controls: {
-            fullscreen: !1
-        }
-    });
-    $(".ilbvid-trampo-2").iLightBox({
-        skin: "springfree",
-        controls: {
-            fullscreen: !1
-        }
-    });
-    $(".ilbvid-trampo-3").iLightBox({
-        skin: "springfree",
-        controls: {
-            fullscreen: !1
-        }
-    });
-    $(".ilbvid-trampo-4").iLightBox({
-        skin: "springfree",
-        controls: {
-            fullscreen: !1
-        }
-    });
-    $(".ilbvid-trampo-5").iLightBox({
-        skin: "springfree",
-        controls: {
-            fullscreen: !1
-        }
-    });
 
-    // ========= Video Testimonials
-    $(".ilbvid-vidTesti-1").iLightBox({
-        skin: "springfree",
-        controls: {
-            fullscreen: !1
-        }
+        $(".ilbvid-trampo-1").iLightBox({
+            skin: "springfree",
+            controls: {
+                fullscreen: !1
+            }
+        });
+        $(".ilbvid-trampo-2").iLightBox({
+            skin: "springfree",
+            controls: {
+                fullscreen: !1
+            }
+        });
+        $(".ilbvid-trampo-3").iLightBox({
+            skin: "springfree",
+            controls: {
+                fullscreen: !1
+            }
+        });
+        $(".ilbvid-trampo-4").iLightBox({
+            skin: "springfree",
+            controls: {
+                fullscreen: !1
+            }
+        });
+        $(".ilbvid-trampo-5").iLightBox({
+            skin: "springfree",
+            controls: {
+                fullscreen: !1
+            }
+        });
+
+        // ========= Video Testimonials
+        $(".ilbvid-vidTesti-1").iLightBox({
+            skin: "springfree",
+            controls: {
+                fullscreen: !1
+            }
+        });
+        $(".ilbvid-vidTesti-2").iLightBox({
+            skin: "springfree",
+            controls: {
+                fullscreen: !1
+            }
+        });
+        $(".ilbvid-vidTesti-3").iLightBox({
+            skin: "springfree",
+            controls: {
+                fullscreen: !1
+            }
+        });
+
     });
-    $(".ilbvid-vidTesti-2").iLightBox({
-        skin: "springfree",
-        controls: {
-            fullscreen: !1
-        }
-    });
-    $(".ilbvid-vidTesti-3").iLightBox({
-        skin: "springfree",
-        controls: {
-            fullscreen: !1
-        }
-    });
-  
-  });
 
 })(jQuery);
--- tgoma/js/planetx.js
+++ tgoma/js/planetx.js
@@ -1,11 +1,285 @@
 // JavaScript Document
 
-$("#inline_html_simple").iLightBox({
-        skin: "springfree",
-        controls: {
-            fullscreen: !1
-        }
+$(document).ready(function() {
+
+    // HTML - Inline Content
+    // $('#game1').iLightBox({
+    //     attr: 'target',
+    //     skin: 'metro-white'
+    // });
+
+   
+    $('#game1, #tab4 #game1').click(function(){
+        $.iLightBox([
+            {
+                URL: '#game_detail1',
+                type: 'inline',
+                options: {
+                    width: 1178,
+                    height: 470
+                }
+            }
+        ],{
+            skin: 'metro-white'
+        });
+
+        setTimeout(function(){
+            jQuery(".ilightbox-wrapper #gameSlide1").owlCarousel({
+                items:1,
+                itemsDesktop : [1200,1],
+                itemsDesktopSmall : [992,1],
+                itemsTablet: [768,1],
+                itemsMobile : [767,1],
+                navigation: true,
+                mouseDrag: true,
+                navigationText : ["<img src='images/icon-prev.png'>","<img src='images/icon-next.png'>"],
+                pagination: false       
+            });
+        },500);
+        return false;
     });
+
+    $('#game2, #tab4 #game2').click(function(){
+        $.iLightBox([
+            {
+                URL: '#game_detail2',
+                type: 'inline',
+                options: {
+                    width: 1178,
+                    height: 470
+                }
+            }
+        ],{
+            skin: 'metro-white'
+        });
+
+        setTimeout(function(){
+            jQuery(".ilightbox-wrapper #gameSlide2").owlCarousel({
+                items:1,
+                itemsDesktop : [1200,1],
+                itemsDesktopSmall : [992,1],
+                itemsTablet: [768,1],
+                itemsMobile : [767,1],
+                navigation: true,
+                mouseDrag: true,
+                navigationText : ["<img src='images/icon-prev.png'>","<img src='images/icon-next.png'>"],
+                pagination: false       
+            });
+        },500);
+        return false;
+    });
+
+     $('#game3, #tab4 #game3').click(function(){
+        $.iLightBox([
+            {
+                URL: '#game_detail3',
+                type: 'inline',
+                options: {
+                    width: 1178,
+                    height: 470
+                }
+            }
+        ],{
+            skin: 'metro-white'
+        });
+
+        setTimeout(function(){
+            jQuery(".ilightbox-wrapper #gameSlide3").owlCarousel({
+                items:1,
+                itemsDesktop : [1200,1],
+                itemsDesktopSmall : [992,1],
+                itemsTablet: [768,1],
+                itemsMobile : [767,1],
+                navigation: true,
+                mouseDrag: true,
+                navigationText : ["<img src='images/icon-prev.png'>","<img src='images/icon-next.png'>"],
+                pagination: false       
+            });
+        },500);
+        return false;
+    });
+
+    $('#game4, #tab4 #game4').click(function(){
+        $.iLightBox([
+            {
+                URL: '#game_detail4',
+                type: 'inline',
+                options: {
+                    width: 1178,
+                    height: 470
+                }
+            }
+        ],{
+            skin: 'metro-white'
+        });
+
+        setTimeout(function(){
+            jQuery(".ilightbox-wrapper #gameSlide4").owlCarousel({
+                items:1,
+                itemsDesktop : [1200,1],
+                itemsDesktopSmall : [992,1],
+                itemsTablet: [768,1],
+                itemsMobile : [767,1],
+                navigation: true,
+                mouseDrag: true,
+                navigationText : ["<img src='images/icon-prev.png'>","<img src='images/icon-next.png'>"],
+                pagination: false       
+            });
+        },500);
+        return false;
+    });
+
+    $('#game5, #tab4 #game5').click(function(){
+        $.iLightBox([
+            {
+                URL: '#game_detail5',
+                type: 'inline',
+                options: {
+                    width: 1178,
+                    height: 470
+                }
+            }
+        ],{
+            skin: 'metro-white'
+        });
+
+        setTimeout(function(){
+            jQuery(".ilightbox-wrapper #gameSlide5").owlCarousel({
+                items:1,
+                itemsDesktop : [1200,1],
+                itemsDesktopSmall : [992,1],
+                itemsTablet: [768,1],
+                itemsMobile : [767,1],
+                navigation: true,
+                mouseDrag: true,
+                navigationText : ["<img src='images/icon-prev.png'>","<img src='images/icon-next.png'>"],
+                pagination: false       
+            });
+        },500);
+        return false;
+    });
+
+    $('#game6, #tab4 #game6').click(function(){
+        $.iLightBox([
+            {
+                URL: '#game_detail6',
+                type: 'inline',
+                options: {
+                    width: 1178,
+                    height: 470
+                }
+            }
+        ],{
+            skin: 'metro-white'
+        });
+
+        setTimeout(function(){
+            jQuery(".ilightbox-wrapper #gameSlide6").owlCarousel({
+                items:1,
+                itemsDesktop : [1200,1],
+                itemsDesktopSmall : [992,1],
+                itemsTablet: [768,1],
+                itemsMobile : [767,1],
+                navigation: true,
+                mouseDrag: true,
+                navigationText : ["<img src='images/icon-prev.png'>","<img src='images/icon-next.png'>"],
+                pagination: false       
+            });
+        },500);
+        return false;
+    });
+
+    $('#game7, #tab4 #game7').click(function(){
+        $.iLightBox([
+            {
+                URL: '#game_detail7',
+                type: 'inline',
+                options: {
+                    width: 1178,
+                    height: 470
+                }
+            }
+        ],{
+            skin: 'metro-white'
+        });
+
+        setTimeout(function(){
+            jQuery(".ilightbox-wrapper #gameSlide7").owlCarousel({
+                items:1,
+                itemsDesktop : [1200,1],
+                itemsDesktopSmall : [992,1],
+                itemsTablet: [768,1],
+                itemsMobile : [767,1],
+                navigation: true,
+                mouseDrag: true,
+                navigationText : ["<img src='images/icon-prev.png'>","<img src='images/icon-next.png'>"],
+                pagination: false       
+            });
+        },500);
+        return false;
+    });
+
+    $('#game8, #tab4 #game8').click(function(){
+        $.iLightBox([
+            {
+                URL: '#game_detail8',
+                type: 'inline',
+                options: {
+                    width: 1178,
+                    height: 470
+                }
+            }
+        ],{
+            skin: 'metro-white'
+        });
+
+        setTimeout(function(){
+            jQuery(".ilightbox-wrapper #gameSlide8").owlCarousel({
+                items:1,
+                itemsDesktop : [1200,1],
+                itemsDesktopSmall : [992,1],
+                itemsTablet: [768,1],
+                itemsMobile : [767,1],
+                navigation: true,
+                mouseDrag: true,
+                navigationText : ["<img src='images/icon-prev.png'>","<img src='images/icon-next.png'>"],
+                pagination: false       
+            });
+        },500);
+        return false;
+    });
+
+    $('#game9, #tab4 #game9').click(function(){
+        $.iLightBox([
+            {
+                URL: '#game_detail9',
+                type: 'inline',
+                options: {
+                    width: 1178,
+                    height: 470
+                }
+            }
+        ],{
+            skin: 'metro-white'
+        });
+
+        setTimeout(function(){
+            jQuery(".ilightbox-wrapper #gameSlide9").owlCarousel({
+                items:1,
+                itemsDesktop : [1200,1],
+                itemsDesktopSmall : [992,1],
+                itemsTablet: [768,1],
+                itemsMobile : [767,1],
+                navigation: true,
+                mouseDrag: true,
+                navigationText : ["<img src='images/icon-prev.png'>","<img src='images/icon-next.png'>"],
+                pagination: false       
+            });
+        },500);
+        return false;
+    });
+});
+
 
 jQuery("#testi-carousel").owlCarousel({
 	items:1,
@@ -20,89 +294,126 @@ jQuery("#testi-carousel").owlCarousel({
 });
 
 jQuery("#award-Slider").owlCarousel({
-		items:2,
-		itemsDesktop : [1200,2],
-		itemsDesktopSmall : [992,2],
-		itemsTablet: [768,2],
-		itemsMobile : [767,1],
-		margin:0,
-		stagePadding:0,
-		navigation: true,
-		mouseDrag: true,
-		navigationText : ["&#xf060;","&#xf061;"],
-		pagination: false		
-	});
+	items:2,
+	itemsDesktop : [1200,2],
+	itemsDesktopSmall : [992,2],
+	itemsTablet: [768,2],
+	itemsMobile : [767,1],
+	margin:0,
+	stagePadding:0,
+	navigation: true,
+	mouseDrag: true,
+	navigationText : ["&#xf060;","&#xf061;"],
+	pagination: false		
+});
+
 	
 	
 	
-$(".game").colorbox({inline:true, width:"100%"});
+/*$(".game").colorbox({inline:true, width:"100%"});
 $(document).bind('cbox_complete', function(){
   setTimeout( function(){	
   var c_height = $('#cboxLoadedContent .tab_popup').outerHeight();
   $('#cboxContent').css('height',c_height);
   $('#cboxLoadedContent, #cboxWrapper, #colorbox').css('height',c_height);
   },500);
-});
+});*/
+
 
 // =========== Counter 
-    var initialDate = new Date("9/10/2015");
-    var jump = 2;
+var initialDate = new Date("9/10/2015");
+var jump = 2;
 
-    var today = new Date();
-    var dd = today.getDate();
-    var mm = today.getMonth() + 1; //January is 0!
-    var yyyy = today.getFullYear();
-    if(dd < 10) {
-        dd='0'+dd
-    } 
-    if(mm < 10) {
-        mm='0'+mm
-    }
-    today = mm+'/'+dd+'/'+yyyy;
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth() + 1; //January is 0!
+var yyyy = today.getFullYear();
+if(dd < 10) {
+    dd='0'+dd
+} 
+if(mm < 10) {
+    mm='0'+mm
+}
+today = mm+'/'+dd+'/'+yyyy;
 
-    
-    var todayDate = new Date(today);
-    var timeDiff = Math.abs(todayDate.getTime() - initialDate.getTime());
-    var diffSeconds = Math.ceil((timeDiff / (1000 * 3600 * 24)) * 86400);
 
-    // -----
-    var initialDate_2 = new Date("9/01/2016");
-    var jump_2 = 1;
+var todayDate = new Date(today);
+var timeDiff = Math.abs(todayDate.getTime() - initialDate.getTime());
+var diffSeconds = Math.ceil((timeDiff / (1000 * 3600 * 24)) * 86400);
 
-    var today_2 = new Date();
-    var dd_2 = today_2.getDate();
-    var mm_2 = today_2.getMonth() + 1; //January is 0!
-    var yyyy_2 = today_2.getFullYear();
-    if(dd_2 < 10) {
-        dd_2='0'+dd_2
-    } 
-    if(mm_2 < 10) {
-        mm_2='0'+mm_2
-    }
-    today_2 = mm_2+'/'+dd_2+'/'+yyyy_2;
+// -----
+var initialDate_2 = new Date("9/01/2016");
+var jump_2 = 1;
 
-    
-    var todayDate_2 = new Date(today_2);
-    var timeDiff_2 = Math.abs(todayDate_2.getTime() - initialDate_2.getTime());
-    var diffSeconds_2 = Math.ceil((timeDiff_2 / (1000 * 3600 * 24)) * 86400);
-    // ----
+var today_2 = new Date();
+var dd_2 = today_2.getDate();
+var mm_2 = today_2.getMonth() + 1; //January is 0!
+var yyyy_2 = today_2.getFullYear();
+if(dd_2 < 10) {
+    dd_2='0'+dd_2
+} 
+if(mm_2 < 10) {
+    mm_2='0'+mm_2
+}
+today_2 = mm_2+'/'+dd_2+'/'+yyyy_2;
 
-    window.setInterval(function(){
-        diffSeconds = parseInt(diffSeconds) + parseInt(jump);
-        var diffSeconds_a = diffSeconds.toString().split('');
-        var jump_count = new Array();
-        for (var i = 0; i <= diffSeconds_a.length - 1; i++) {
-            jump_count.push('<span>'+ diffSeconds_a[i] +'</span>');              
+
+var todayDate_2 = new Date(today_2);
+var timeDiff_2 = Math.abs(todayDate_2.getTime() - initialDate_2.getTime());
+var diffSeconds_2 = Math.ceil((timeDiff_2 / (1000 * 3600 * 24)) * 86400);
+// ----
+
+window.setInterval(function(){
+    diffSeconds = parseInt(diffSeconds) + parseInt(jump);
+    var diffSeconds_a = diffSeconds.toString().split('');
+    var jump_count = new Array();
+    var jump_count_old = new Array();
+
+    $('#jumpCount span').each(function(index, el) {
+        jump_count_old.push($(this).text());
+    });
+    for (var i = 0; i <= diffSeconds_a.length - 1; i++) {
+        if(diffSeconds_a[i] == jump_count_old[i]){
+            jump_count.push('<span>'+ diffSeconds_a[i] +'</span>');
+        }else{
+            jump_count.push('<span class="flip">'+jump_count_old[i]+'</span>');     
         }
-        $('#jumpCount').html(jump_count);
-
-        diffSeconds_2 = parseInt(diffSeconds_2) + parseInt(jump);
-        var diffSeconds_a_2 = diffSeconds_2.toString().split('');
-        var jump_count_2 = new Array();
-        for (var i = 0; i <= diffSeconds_a_2.length - 1; i++) {
-            jump_count_2.push('<span>'+ diffSeconds_a_2[i] +'</span>');              
-        }
-        $('#caloriCount').html(jump_count_2);
-    }, 1000); 
+    }
+    $('#jumpCount').html(jump_count);
+    setTimeout(function(){
+        $('#jumpCount span').each(function(index, el) {
+            $(this).text(diffSeconds_a[index]);
+        });
+    },300);
     
-    // ====================================
+
+    diffSeconds_2 = parseInt(diffSeconds_2) + parseInt(jump);
+    var diffSeconds_a_2 = diffSeconds_2.toString().split('');
+    var jump_count_2 = new Array();
+    var jump_count_old_2 = new Array();
+
+    $('#caloriCount span').each(function(index, el) {
+        jump_count_old_2.push($(this).text());
+    });
+    for (var i = 0; i <= diffSeconds_a_2.length - 1; i++) {
+        if(diffSeconds_a_2[i] == jump_count_old_2[i]){
+            jump_count_2.push('<span>'+ diffSeconds_a_2[i] +'</span>');
+        }else{
+            jump_count_2.push('<span class="flip">'+ jump_count_old_2[i] +'</span>');     
+        }         
+    }
+    $('#caloriCount').html(jump_count_2);
+    setTimeout(function(){
+        $('#caloriCount span').each(function(index, el) {
+            $(this).text(diffSeconds_a_2[index]);
+        });
+    },300);
+}, 1000); 
+
+// ====================================
+
+
+$(window).resize(function(event) {
+    var get_height = $('.ilightbox-wrapper .owl-item').height()();
+    $('.ilightbox-wrapper iframe').height(get_height);
+});
