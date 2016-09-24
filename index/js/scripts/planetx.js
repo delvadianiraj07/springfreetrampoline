c46ffb3f2a20ababb7d717f7af39affcd5771bad
Niraj <delvadia_niraj@yahoo.com>
2016-09-24 10:38:37 +0530

    Flip counter setup for index and tgoma page

--- index/js/scripts/planetx.js
+++ index/js/scripts/planetx.js
@@ -7,7 +7,7 @@
 
         stickHeader();
 
-        function stickHeader(){
+        function stickHeader() {
             var s = $("#header .header-holder");
             var pos = s.position();
 
@@ -29,8 +29,8 @@
                 }
             });
         }
-        
-        
+
+
 
         // ============= header top banner =================
         // set cookie for banner for first visit
@@ -101,13 +101,14 @@
                 /*$(this).find('.hotspot-tooltip').click(function(ev) {
                    ev.stopPropagation();
                 });*/
-				$(this).find('.hotspot-tooltip').hover(
-				  function() {
-					
-				  }, function() {
-					$(this).parents('.hotspot-point').removeClass('open-preview');
-				  }
-				);
+                $(this).find('.hotspot-tooltip').hover(
+                    function() {
+
+                    },
+                    function() {
+                        $(this).parents('.hotspot-point').removeClass('open-preview');
+                    }
+                );
                 //event.stopPropagation();
             });
             var hs_item = $(this).find('.hotspot-tooltip').html();
@@ -120,97 +121,9 @@
         });
 
 
-        // =========== Counter 
-    var initialDate = new Date("9/10/2015");
-    var jump = 2;
 
-    var today = new Date();
-    var dd = today.getDate();
-    var mm = today.getMonth() + 1; //January is 0!
-    var yyyy = today.getFullYear();
-    if(dd < 10) {
-        dd='0'+dd
-    } 
-    if(mm < 10) {
-        mm='0'+mm
-    }
-    today = mm+'/'+dd+'/'+yyyy;
 
-    
-    var todayDate = new Date(today);
-    var timeDiff = Math.abs(todayDate.getTime() - initialDate.getTime());
-    var diffSeconds = Math.ceil((timeDiff / (1000 * 3600 * 24)) * 86400);
-
-    // -----
-    var initialDate_2 = new Date("9/01/2016");
-    var jump_2 = 1;
-
-    var today_2 = new Date();
-    var dd_2 = today_2.getDate();
-    var mm_2 = today_2.getMonth() + 1; //January is 0!
-    var yyyy_2 = today_2.getFullYear();
-    if(dd_2 < 10) {
-        dd_2='0'+dd_2
-    } 
-    if(mm_2 < 10) {
-        mm_2='0'+mm_2
-    }
-    today_2 = mm_2+'/'+dd_2+'/'+yyyy_2;
-
-    
-    var todayDate_2 = new Date(today_2);
-    var timeDiff_2 = Math.abs(todayDate_2.getTime() - initialDate_2.getTime());
-    var diffSeconds_2 = Math.ceil((timeDiff_2 / (1000 * 3600 * 24)) * 86400);
-    // ----
-
-   // window.setInterval(function(){
-        diffSeconds = parseInt(diffSeconds) + parseInt(jump);
-        var diffSeconds_a = diffSeconds.toString().split('');
-        var jump_count = new Array();
-        var jump_count_old = new Array();
-
-        $('#jumpCount span').each(function(index, el) {
-            jump_count_old.push($(this).text());
-        });
-        for (var i = 0; i <= diffSeconds_a.length - 1; i++) {
-            if(diffSeconds_a[i] == jump_count_old[i]){
-                jump_count.push('<span>'+ diffSeconds_a[i] +'</span>');
-            }else{
-                jump_count.push('<span class="flip">'+jump_count_old[i]+'</span>');     
-            }
-        }
-        $('#jumpCount').html(jump_count);
-        setTimeout(function(){
-            $('#jumpCount span').each(function(index, el) {
-                $(this).text(diffSeconds_a[index]);
-            });
-        },300);
-        
-
-        diffSeconds_2 = parseInt(diffSeconds_2) + parseInt(jump);
-        var diffSeconds_a_2 = diffSeconds_2.toString().split('');
-        var jump_count_2 = new Array();
-        var jump_count_old_2 = new Array();
-
-        $('#caloriCount span').each(function(index, el) {
-            jump_count_old_2.push($(this).text());
-        });
-        for (var i = 0; i <= diffSeconds_a_2.length - 1; i++) {
-            if(diffSeconds_a_2[i] == jump_count_old_2[i]){
-                jump_count_2.push('<span>'+ diffSeconds_a_2[i] +'</span>');
-            }else{
-                jump_count_2.push('<span class="flip">'+ jump_count_old_2[i] +'</span>');     
-            }         
-        }
-        $('#caloriCount').html(jump_count_2);
-        setTimeout(function(){
-            $('#caloriCount span').each(function(index, el) {
-                $(this).text(diffSeconds_a_2[index]);
-            });
-        },300);
-    //}, 1000); 
-    
-    // ====================================
+        // ====================================
 
 
         $("#trmp-hotspots .ilbvid-trampo-1").iLightBox({
@@ -218,8 +131,7 @@
             controls: {
                 fullscreen: !1
             }
-        });		
-		
+        });
         $("#trmp-hotspots .ilbvid-trampo-2").iLightBox({
             skin: "springfree",
             controls: {
@@ -244,37 +156,37 @@
                 fullscreen: !1
             }
         });
-		
-		
-		$("#hotspots-item .ilbvid-trampo-1").iLightBox({
+
+
+        $("#hotspots-item .ilbvid-trampo-1").iLightBox({
             skin: "springfree",
             controls: {
                 fullscreen: !1
             }
         });
-		
-		$("#hotspots-item .ilbvid-trampo-2").iLightBox({
+
+        $("#hotspots-item .ilbvid-trampo-2").iLightBox({
             skin: "springfree",
             controls: {
                 fullscreen: !1
             }
         });
-		
-		$("#hotspots-item .ilbvid-trampo-3").iLightBox({
+
+        $("#hotspots-item .ilbvid-trampo-3").iLightBox({
             skin: "springfree",
             controls: {
                 fullscreen: !1
             }
         });
-		
-		$("#hotspots-item .ilbvid-trampo-4").iLightBox({
+
+        $("#hotspots-item .ilbvid-trampo-4").iLightBox({
             skin: "springfree",
             controls: {
                 fullscreen: !1
             }
         });
-		
-		$("#hotspots-item .ilbvid-trampo-5").iLightBox({
+
+        $("#hotspots-item .ilbvid-trampo-5").iLightBox({
             skin: "springfree",
             controls: {
                 fullscreen: !1
@@ -303,17 +215,17 @@
 
     });
 
-    // Make the flip counter
-    $("#flipcounter").flipCounterInit({'speed': 0.5});
+    // ---------- Make the flip counter for Jump
+    $("#flipcounterJump").flipCounterInit({ 'speed': 0.5 });
 
     var startTime = new Date().getTime();
-    var initialDate = new Date("9/10/2015").getTime();
+    var initialDate_jump = new Date("9/10/2015").getTime();
 
     // Update values
     function updateLoop() {
-        var elapsedTime = new Date().getTime() - initialDate;
-        var diffSeconds = Math.ceil((elapsedTime / (1000 * 3600 * 24)) * 86400);
-        $("#flipcounter").flipCounterUpdate(diffSeconds);
+        var elapsedTime_jump = new Date().getTime() - initialDate_jump;
+        var diffSeconds_jump = Math.ceil((elapsedTime_jump / (1000 * 3600 * 24)) * 86400);
+        $("#flipcounterJump").flipCounterUpdate(diffSeconds_jump);
         window.setTimeout(function() {
             updateLoop();
         }, 43);
@@ -321,5 +233,23 @@
 
     // do it!
     updateLoop();
+
+    // ----------- Make the flip counter Calories
+    $("#flipcounterCalori").flipCounterInit({ 'speed': 0.5 });
+
+    var initialDate_cal = new Date("9/5/2016").getTime();
+
+    // Update values
+    function updateLoop2() {
+        var elapsedTime_cal = new Date().getTime() - initialDate_cal;
+        var diffSeconds_cal = Math.ceil((elapsedTime_cal / (1000 * 3600 * 24)) * 86400);
+        $("#flipcounterCalori").flipCounterUpdate(diffSeconds_cal);
+        window.setTimeout(function() {
+            updateLoop2();
+        }, 1000);
+    }
+
+    // do it!
+    updateLoop2();
 
 })(jQuery);
--- tgoma/js/planetx.js
+++ tgoma/js/planetx.js
@@ -325,94 +325,42 @@ $(document).bind('cbox_complete', function(){
 
 
 // =========== Counter 
-var initialDate = new Date("9/10/2015");
-var jump = 2;
+ // ---------- Make the flip counter for Jump
+    $("#flipcounterJump").flipCounterInit({ 'speed': 0.5 });
 
-var today = new Date();
-var dd = today.getDate();
-var mm = today.getMonth() + 1; //January is 0!
-var yyyy = today.getFullYear();
-if(dd < 10) {
-    dd='0'+dd
-} 
-if(mm < 10) {
-    mm='0'+mm
-}
-today = mm+'/'+dd+'/'+yyyy;
+    var startTime = new Date().getTime();
+    var initialDate_jump = new Date("9/10/2015").getTime();
 
-
-var todayDate = new Date(today);
-var timeDiff = Math.abs(todayDate.getTime() - initialDate.getTime());
-var diffSeconds = Math.ceil((timeDiff / (1000 * 3600 * 24)) * 86400);
-
-// -----
-var initialDate_2 = new Date("9/01/2016");
-var jump_2 = 1;
-
-var today_2 = new Date();
-var dd_2 = today_2.getDate();
-var mm_2 = today_2.getMonth() + 1; //January is 0!
-var yyyy_2 = today_2.getFullYear();
-if(dd_2 < 10) {
-    dd_2='0'+dd_2
-} 
-if(mm_2 < 10) {
-    mm_2='0'+mm_2
-}
-today_2 = mm_2+'/'+dd_2+'/'+yyyy_2;
-
-
-var todayDate_2 = new Date(today_2);
-var timeDiff_2 = Math.abs(todayDate_2.getTime() - initialDate_2.getTime());
-var diffSeconds_2 = Math.ceil((timeDiff_2 / (1000 * 3600 * 24)) * 86400);
-// ----
-
-window.setInterval(function(){
-    diffSeconds = parseInt(diffSeconds) + parseInt(jump);
-    var diffSeconds_a = diffSeconds.toString().split('');
-    var jump_count = new Array();
-    var jump_count_old = new Array();
-
-    $('#jumpCount span').each(function(index, el) {
-        jump_count_old.push($(this).text());
-    });
-    for (var i = 0; i <= diffSeconds_a.length - 1; i++) {
-        if(diffSeconds_a[i] == jump_count_old[i]){
-            jump_count.push('<span>'+ diffSeconds_a[i] +'</span>');
-        }else{
-            jump_count.push('<span class="flip">'+jump_count_old[i]+'</span>');     
-        }
+    // Update values
+    function updateLoop() {
+        var elapsedTime_jump = new Date().getTime() - initialDate_jump;
+        var diffSeconds_jump = Math.ceil((elapsedTime_jump / (1000 * 3600 * 24)) * 86400);
+        $("#flipcounterJump").flipCounterUpdate(diffSeconds_jump);
+        window.setTimeout(function() {
+            updateLoop();
+        }, 43);
     }
-    $('#jumpCount').html(jump_count);
-    setTimeout(function(){
-        $('#jumpCount span').each(function(index, el) {
-            $(this).text(diffSeconds_a[index]);
-        });
-    },300);
-    
 
-    diffSeconds_2 = parseInt(diffSeconds_2) + parseInt(jump);
-    var diffSeconds_a_2 = diffSeconds_2.toString().split('');
-    var jump_count_2 = new Array();
-    var jump_count_old_2 = new Array();
+    // do it!
+    updateLoop();
 
-    $('#caloriCount span').each(function(index, el) {
-        jump_count_old_2.push($(this).text());
-    });
-    for (var i = 0; i <= diffSeconds_a_2.length - 1; i++) {
-        if(diffSeconds_a_2[i] == jump_count_old_2[i]){
-            jump_count_2.push('<span>'+ diffSeconds_a_2[i] +'</span>');
-        }else{
-            jump_count_2.push('<span class="flip">'+ jump_count_old_2[i] +'</span>');     
-        }         
+    // ----------- Make the flip counter Calories
+    $("#flipcounterCalori").flipCounterInit({ 'speed': 0.5 });
+
+    var initialDate_cal = new Date("9/5/2016").getTime();
+
+    // Update values
+    function updateLoop2() {
+        var elapsedTime_cal = new Date().getTime() - initialDate_cal;
+        var diffSeconds_cal = Math.ceil((elapsedTime_cal / (1000 * 3600 * 24)) * 86400);
+        $("#flipcounterCalori").flipCounterUpdate(diffSeconds_cal);
+        window.setTimeout(function() {
+            updateLoop2();
+        }, 1000);
     }
-    $('#caloriCount').html(jump_count_2);
-    setTimeout(function(){
-        $('#caloriCount span').each(function(index, el) {
-            $(this).text(diffSeconds_a_2[index]);
-        });
-    },300);
-}, 1000); 
+
+    // do it!
+    updateLoop2();
 
 // ====================================
 
